0acc4ba1150acc99876a08150f9743000a0b6a1f
dwolf <910063@gmail.com>
2019-07-21 02:57:12 +0800

    mainpageOK

--- MCLABDemo01/MCLABDemo01/ViewController.swift
+++ MCLABDemo01/MCLABDemo01/ViewController.swift
@@ -7,14 +7,28 @@
 //
 
 import UIKit
+import Foundation
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mainTopBar: UINavigationItem!
+    @IBOutlet weak var mainBackground: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        //設定背景透明度
+        mainBackground.alpha = 0.75
+        //top bar設定
+        //navigationController?.navigationBar.barTintColor = UIColor(red: 118/255, green: 184/255, blue: 188/255, alpha: 0.01)
+        let alphaBackgroundImage = UIImage()
+        self.navigationController?.navigationBar.setBackgroundImage(
+            alphaBackgroundImage,for: .default)
     }
-
+    
+    //開始拍照or選照片
+    @IBAction func startButton(_ sender: Any) {
+    }
 
 }
 
